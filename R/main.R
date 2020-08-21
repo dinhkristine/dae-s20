f22779d9b174cca86e6b0799202b113514bc95e1
Kristine Dinh <kdinh@arrowheadgrp.com>
2020-08-21 12:18:24 -0700

    finalized report

--- R/main.R
+++ R/main.R
@@ -38,7 +38,17 @@ fire %<>%
 fire %<>%
   mutate(large_fire = case_when(area == 0 ~ 0, TRUE ~ 1))
   
+print_summary_table <- list()
+for (i in setdiff(colnames(fire), c("X", "Y", "month", "large_fire"))){
+  sum_tab <- SummaryTable(i)
+  print_summary_table[[i]] <- sum_tab
+}
 
+print_summary_table %<>% bind_rows() 
+print_summary_table %<>% modify_if(is.numeric, round, 4)
+
+
+write.csv(print_summary_table, "data/summary-table.CSV")
 
 fire %<>%
   mutate(FFMC_bin = ntile(FFMC, 10), 
@@ -198,25 +208,56 @@ test$percentile <- ntile(test$area_pred, 10)
 
 em_pred_df <- test %>% 
   group_by(percentile) %>% 
-  summarise(em_prop = mean(large_fire), 
-            pred_prop = mean(area_pred))
+  summarise("Actual" = mean(large_fire), 
+            "Predict" = mean(area_pred))
 
-ggplot(em_pred_df, aes(x = percentile)) + 
-  geom_line(aes(y = em_prop), color = "red", size = 1) + 
-  geom_line(aes(y = pred_prop), color = "blue", size = 1) + 
-  geom_point(aes(y = em_prop), color = "red", size = 2) + 
-  geom_point(aes(y = pred_prop), color = "blue", size = 2) + 
-  theme_minimal()
+em_pred_df %<>% 
+  gather("key", "value", -percentile)
+
+ggplot(em_pred_df, aes(x = factor(percentile), y = value, color = key, group = key)) + 
+  geom_line(size = 1) + 
+  geom_point(size = 2) + 
+  theme_minimal() + 
+  labs(x = "Percentile", 
+       y = "Average Proportion of of Large Fire") + 
+  theme(legend.title = element_blank()) + 
+  scale_color_manual(values = c("black", "#9aa1d9"))
 
 aic1 <- fit$aic
 test_roc <- pROC::roc(response = test$large_fire, predictor = test$area_pred)
 test_auc1 <- as.data.frame(pROC::auc(test_roc))
 
 
-broom::tidy(fit) %>% 
-  cbind(confint(fit))
+tidy <- broom::tidy(fit) %>% 
+  cbind(exp(confint(fit))) 
+
+tidy %<>% mutate(OR = exp(fit$coefficients)) %>%  modify_if(is.numeric, round, 4) 
+tidy
+
+exp(tidy$estimate[[1]] + qnorm(c(0.025,0.975)) * tidy$estimate[[1]])
+exp(tidy$estimate[[2]] + qnorm(c(0.025,0.975)) * tidy$estimate[[2]])
+exp(tidy$estimate[[3]] + qnorm(c(0.025,0.975)) * tidy$estimate[[3]])
 
 test_auc1
+summary(fit)
+
+aod::wald.test(b = coef(fit), Sigma = vcov(fit), Terms = 4)
+
+tidy 
+write_csv(tidy, "data/tidy-hazard-yflag.CSV")
+
+
+test %>% 
+  group_by(round(area_pred), large_fire) %>% 
+  tally() %>% 
+  
+tidy
+
+test %<>% mutate(area_pred = case_when(area_pred < 0.5 ~ 0.5, TRUE ~ 1.1))
+
+confusion_matrix <- ftable(test$large_fire, test$area_pred)
+accuracy <- sum(diag(confusion_matrix))/nrow(test)*100
+accuracy
 
 #### Interaction ####
 
@@ -244,21 +285,97 @@ fit <- glm(formula = large_fire ~ hazard + Y_flag,
            weights = fire_count,
            data    = train)
 
-standard_res <- rstandard(fit, type = "pearson")
+# standard_res <- rstandard(fit, type = "pearson")
+# 
+# plot(standard_res)
+# 
+# car::vif(fit)
+# 
+# car::outlierTest(fit)
+# library(broom)
+# 
+# fit_df <- augment(fit) %>% mutate(ID = 1:nrow(.))
+# 
+# ggplot(fit_df, aes(x = ID, y = .std.resid)) + 
+#   geom_point(aes(color = factor(large_fire)), alpha = 0.5) + 
+#   theme_minimal()
+# 
+# plot(fit, which = 4, id.n = 3)
 
-plot(standard_res)
 
-car::vif(fit)
+library(car)
 
-car::outlierTest(fit)
-library(broom)
+outlierTest(fit) # Bonferonni p-value for most extreme obs
 
-fit_df <- augment(fit) %>% mutate(ID = 1:nrow(.))
 
-ggplot(fit_df, aes(x = ID, y = .std.resid)) + 
-  geom_point(aes(color = factor(large_fire)), alpha = 0.5) + 
-  theme_minimal()
+# Influential Observations
+# Cook's D plot
+# identify D values > 4/(n-k-1)
+# cutoff <- 4/((nrow(fire)-length(fit$coefficients)-2))
+# plot(fit, which=4, cook.levels=cutoff)
+# Influence Plot
+influencePlot(fit, id.method="identify", main="Influence Plot", sub="Circle size is proportial to Cook's Distance" )
 
-plot(fit, which = 4, id.n = 3)
 
+diag <- augment(fit) %>% mutate(Index = 1:nrow(.))
+
+diag %<>% 
+  mutate(high_cooksd = case_when(
+    .cooksd > .15 ~ 1, TRUE ~ 0), 
+    col_stdresid = case_when(
+      .std.resid > 0 ~ 1, 
+      .std.resid < 0 ~ 0), 
+    high_hat = case_when(
+      .hat > .1 ~ 1, 
+      TRUE ~ 0
+    ))
+
+
+ggplot(diag, aes(x = Index, y = .cooksd, color = factor(high_cooksd))) + 
+  geom_point() +
+  labs(y = "Cook's Distance") + 
+  theme_minimal() + 
+  scale_color_manual(values = c("#9aa1d9", "black")) + 
+  theme(legend.position = "none") 
+
+ggplot(diag, aes(x = Index, y = .std.resid, color = factor(col_stdresid))) + 
+  geom_point() +
+  labs(y = "Standard Residuals") + 
+  theme_minimal() + 
+  scale_color_manual(values = c("black", "#9aa1d9")) + 
+  theme(legend.position = "none")
+
+ggplot(diag, aes(x = Index, y = .hat, color = factor(high_hat))) + 
+  geom_point() +
+  labs(y = "Diagonal Hat Matrix") + 
+  theme_minimal() + 
+  scale_color_manual(values = c("#9aa1d9", "black")) + 
+  theme(legend.position = "none")
+
+
+library(MASS)
+sresid <- studres(fit)
+hist(sresid, freq=FALSE,
+     main="Distribution of Studentized Residuals")
+xfit<-seq(min(sresid),max(sresid),length=40)
+yfit<-dnorm(xfit)
+lines(xfit, yfit)
+
+
+# Evaluate homoscedasticity
+# plot studentized residuals vs. fitted values
+spreadLevelPlot(fit)
+
+
+# Evaluate Collinearity
+vif(fit) # variance inflation factors
+sqrt(vif(fit)) > 2 # problem?
+
+
+inf_points <- (diag %>% filter(.std.resid > 2 | .std.resid < -2))$.rownames %>% as.numeric()
+
+fire[inf_points, ] %>% as.data.frame() %>% dplyr::select(large_fire, hazard, Y_flag, fire_count) %>% 
+  mutate(rownames = inf_points)
+
+plot(diag$.resid, diag$large_fire)
 
